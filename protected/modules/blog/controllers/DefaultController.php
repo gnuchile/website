4e193effe7bb160c4098afdb6cc7b5c5e6dba86c
Tomás Solar Castro <tsolar@gnuchile.cl>
2012-08-15 01:24:31 -0400

    controlador por defecto del blog, maneja el listado de todos los posts que son de blogs, y el listado de los blogs de los usuarios

---
+++ protected/modules/blog/controllers/DefaultController.php
@@ -0,0 +1,46 @@
+<?php
+
+class DefaultController extends Controller
+{
+
+    public function actionIndex()
+    {
+        $posts = Post::model()->inBlog()->findAll();
+
+        $this->render('index', array(
+            'posts' => $posts,
+                )
+        );
+    }
+
+    public function actionView()
+    {
+        $username = NULL;
+
+        if (isset($_GET['username']))
+        {
+            $username = $_GET['username'];
+        }
+        else
+        {
+            throw new CException('username not set.');
+        }
+
+        $user = User::model()->findByAttributes(array('username' => $username));
+
+        if ($user)
+        {
+            $userBlogPosts = $user->blogPosts;
+            $this->render('view', array(
+                'model' => $user,
+                'posts' => $userBlogPosts,
+                    )
+            );
+        }
+        else
+        {
+            throw new CHttpException(404, 'No existe el blog de ' . $username);
+        }
+    }
+
+}
