21fc4f569737c131d4a9e0996ca5ecd36395fcf5
Tomás Solar Castro <tsolar@gnuchile.cl>
2012-08-14 23:52:36 -0400

    se agregan scopes

--- protected/models/Post.php
+++ protected/models/Post.php
@@ -4,49 +4,53 @@ Yii::import('application.models._base.BasePost');
 
 class Post extends BasePost
 {
-	public static function model($className=__CLASS__) {
-		return parent::model($className);
-	}
 
-        protected function beforeValidate()
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    protected function beforeValidate()
+    {
+        $userID = Yii::app()->user->id;
+        $user = User::model()->findByAttributes(array('username' => $userID));
+
+        $createdAt = date('Y-m-d');
+
+        if ($user)
         {
-         
-            $userID = Yii::app()->user->id;
-            $user = User::model()->findByAttributes(array('username'=>$userID));
-            
-            $createdAt = date('Y-m-d');
-
-            if($user)
-            {
-                $this->user_id = $user->id;
-                $this->created_at = $createdAt;
-            }
-
-            var_dump($this->attributes);//exit;
-            return parent::beforeValidate();
-
-            //return parent::beforeSave();
-   
-            //return true;
+            $this->user_id = $user->id;
+            $this->created_at = $createdAt;
         }
 
-        protected function afterSave() {
-            parent::afterSave();
-            //copiar a soap server
-        }
+        return parent::beforeValidate();
+    }
 
-        public function scopes()
-        {
-            return array(
-//                'userBlog'
-            );
-        }
+    protected function afterSave()
+    {
+        parent::afterSave();
+        //copiar a soap server
+    }
 
-        public function getBlogPosts($userID)
-        {
-            return Post::model()->findAllByAttributes(array(
-                'user_id'=>$userID,
-                'in_blog'=>1
-            ));
-        }
+    public function scopes()
+    {
+        return array(
+            'inBlog' => array(
+                'condition' => 'in_blog=1',
+            ),
+            'recently'  => array(
+                'order' => 'create_time DESC',
+                'limit' => 5,
+            ),
+        );
+    }
+
+    public function getBlogPosts($userID)
+    {
+        return Post::model()->findAllByAttributes(array(
+                    'user_id' => $userID,
+                    'in_blog' => 1
+                ));
+    }
+
 }
