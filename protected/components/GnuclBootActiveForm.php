871b307e94e052405946ddf2bfb544d9bfd8afdf
Tomás Solar Castro <tsolar@gnuchile.cl>
2012-08-15 01:03:34 -0400

    se agrega nuevo componente para poder mezclar BootActiveForm y GxActiveForm

---
+++ protected/components/GnuclBootActiveForm.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of GnuclBootActiveForm
+ *
+ * @author tom
+ */
+
+Yii::import('bootstrap.widgets.BootActiveForm');
+
+class GnuclBootActiveForm extends BootActiveForm
+{
+    //put your code here
+}
+
+?>
