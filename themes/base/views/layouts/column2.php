5b3171f625d90e8c82741aeddc857de4b0710f01
Tomás Solar Castro <tsolar@gnuchile.cl>
2012-08-16 22:51:07 -0400

    menú con EBootstrapSidebar :)

--- themes/base/views/layouts/column2.php
+++ themes/base/views/layouts/column2.php
@@ -5,14 +5,20 @@
 <div class="span3">
 	<div id="sidebar">
 	<?php
-		$this->beginWidget('zii.widgets.CPortlet', array(
-			'title'=>'Operations',
-		));
-		$this->widget('zii.widgets.CMenu', array(
+
+//		$this->beginWidget('zii.widgets.CPortlet', array(
+//			'title'=>'Operations',
+////			'htmlOptions'=>array('class'=>'sidebar-nav'),
+//		));
+
+        // se modifica $this->menu para adecuarlo a EBoostrapSidebar :)
+        $this->menu = array(array('label'=>'Operations', 'items'=>$this->menu));
+
+        $this->widget('EBootstrapSidebar', array(
 			'items'=>$this->menu,
 			'htmlOptions'=>array('class'=>'operations'),
 		));
-		$this->endWidget();
+//		$this->endWidget();
 	?>
 	</div><!-- sidebar -->
 </div>
