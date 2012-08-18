ffd63df6357e0116639ff1df6096092b7f6444a8
Tomás Solar Castro <tsolar@gnuchile.cl>
2012-08-18 01:38:12 -0400

    se agrega la vista del blog mejorada :)

---
+++ protected/modules/blog/views/default/_view2.php
@@ -0,0 +1,34 @@
+<?php
+$this->widget('EBootstrapListViewItem', array(
+    'data' => $data,
+    'attributes'=>array(
+        array(
+            'name' => 'title',
+            'template'=>"<div class=\"{class}\"><span class=\"bootstrap-list-view-item-value\">{value}</span></div>\n",
+            'type' => 'html',
+            'cssClass' => 'bootstrap-list-view-item-title',
+            'value' => EBootstrap::link(EBootstrap::encode($data->title), array('//post/view','id'=>$data->id))
+        ),
+//        'body',
+//        'visits',
+        array(
+            'template'=>"<div class=\"{class}\"><span class=\"bootstrap-list-view-item-value\">{value}</span></div>\n",
+            'type' => 'html',
+            'value'=>$data->getCategoriesAsLabel(),
+//            'cssClass'=>'post',
+        ),
+        array(
+            'template'=>"<div class=\"{class}\"><span class=\"bootstrap-list-view-item-value\">{value}</span></div>\n",
+            'type' => 'html',
+            'value'=>$data->user ? $data->user->username : 'nobody\'s post',
+//            'cssClass'=>'post',
+        ),
+        array(
+            'template'=>"<div class=\"{class}\"><span class=\"bootstrap-list-view-item-value\">{value}</span></div>\n",
+            'type' => 'html',
+            'value'=>$data->getMarkdownBody(),
+            'cssClass'=>'post',
+        )
+    ),
+));
+?>
