1fb7bca8beed3092f4d05eb7cb1361be505a88da
Tomás Solar Castro <tsolar@gnuchile.cl>
2012-08-30 22:03:32 -0400

    cambios menores en el form de contacto

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -63,14 +63,14 @@ class SiteController extends Controller
 			if($model->validate())
 			{
 				$name='=?UTF-8?B?'.base64_encode($model->name).'?=';
-				$subject='=?UTF-8?B?'.base64_encode($model->subject).'?=';
+				$subject='=?UTF-8?B?'.base64_encode('[Contacto GNU.cl] '.$model->subject).'?=';
 				$headers="From: $name <{$model->email}>\r\n".
 					"Reply-To: {$model->email}\r\n".
 					"MIME-Version: 1.0\r\n".
 					"Content-type: text/plain; charset=UTF-8";
 
 				mail(Yii::app()->params['adminEmail'],$subject,$model->body,$headers);
-				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
+				Yii::app()->user->setFlash('contact','Gracias por contactarnos, '.$model->name.'. Nos contactaremos a la brevedad posible!');
 				$this->refresh();
 			}
 		}
