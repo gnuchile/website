d7547b780521f06e7ebd36901e283d1fcf8f3fcb
Tomás Solar Castro <tsolar@gnuchile.cl>
2012-08-30 23:45:44 -0400

    cambio en la vista del form de post

--- protected/views/post/_form.php
+++ protected/views/post/_form.php
@@ -1,114 +1,111 @@
-<div class="well">
 <div class="form">
 
 
-<?php $form = $this->beginWidget('GxActiveForm', array(
-	'id' => 'post-form',
-	'enableAjaxValidation' => false,
-        'type'=>'horizontal',
-        'htmlOptions'=>array(
-           // 'class'=>'form-horizontal',
-        )
-));
-?>
-    <fieldset>
-        <legend>
-	<p class="note">
-		Fields with <span class="required">*</span> are required.
-	</p>
-        </legend>
+    <?php
+    $form = $this->beginWidget('EBootstrapActiveForm', array(
+        'id'                   => 'post-form',
+        'enableAjaxValidation' => false,
+        'horizontal'           => true,
+            ));
+    ?>
 
-	<?php echo $form->errorSummary($model); ?>
-        <div class="control-group">
-            <div class="span4">
-		<div class="row">
-		<?php // echo $form->labelEx($model,'user_id'); ?>
-		<?php // echo $form->dropDownList($model, 'user_id', GxHtml::listDataEx(User::model()->findAllAttributes(null, true))); ?>
-		<?php // echo $form->error($model,'user_id'); ?>
-		</div><!-- row -->
-                <?php //echo $form->dropDownListRow($model,'user_id',GxHtml::listDataEx(User::model()->findAllAttributes(null, true)),array('class'=>'span5','maxlength'=>255, 'prompt'=>'lala...')); ?>
-                <?php echo $form->textFieldRow($model,'title',array('class'=>'span5','maxlength'=>255)); ?>
-		<div class="row">
-		<?php //echo $form->labelEx($model,'title'); ?>
-		<?php //echo $form->textField($model, 'title', array('maxlength' => 200)); ?>
-		<?php //echo $form->error($model,'title'); ?>
+    <p class="note">
+        Fields with <span class="required">*</span> are required.
+    </p>
 
-		</div><!-- row -->
-                <?php // echo $form->textAreaRow($model,'body',array('class'=>'span5')); ?>
-        <?php $this->widget(
-    'application.extensions.ddeditor.DDEditor',
-    array(
-        'model'=>$model,
-        'attribute'=>'body',
-        'htmlOptions'=>array('rows'=>10, 'cols'=>70),
-        'previewRequest'=>'post/preview')); ?>
+<?php echo $form->errorSummary($model); ?>
 
-		<div class="row">
-		<?php // echo $form->labelEx($model,'body'); ?>
-		<?php // echo $form->textArea($model, 'body'); ?>
-		<?php // echo $form->error($model,'body'); ?>
-		</div><!-- row -->
-		<div class="row">
-		<?php // echo $form->labelEx($model,'status'); ?>
-		<?php // echo $form->textField($model, 'status', array('maxlength' => 45)); ?>
-		<?php // echo $form->error($model,'status'); ?>
-		</div><!-- row -->
+    <!--		<div class="row">
+    <?php echo $form->beginControlGroup($model, 'user_id'); ?>
+    <?php echo $form->labelEx($model, 'user_id'); ?>
+    <?php echo $form->beginControls(); ?>
+    <?php echo $form->dropDownList($model, 'user_id', GxHtml::listDataEx(User::model()->findAllAttributes(null, true))); ?>
+    <?php echo $form->error($model, 'user_id'); ?>
+    <?php echo $form->endControls(); ?>
+    <?php echo $form->endControlGroup(); ?>
+            </div> row -->
+    <div class="row">
+        <?php echo $form->beginControlGroup($model, 'title'); ?>
+        <?php echo $form->labelEx($model, 'title'); ?>
+        <?php echo $form->beginControls(); ?>
+        <?php echo $form->textField($model, 'title', array('maxlength' => 200)); ?>
+        <?php echo $form->error($model, 'title'); ?>
+        <?php echo $form->endControls(); ?>
+        <?php echo $form->endControlGroup(); ?>
+    </div><!-- row -->
+    <div class="row">
+        <?php echo $form->beginControlGroup($model, 'body'); ?>
+        <?php echo $form->labelEx($model, 'body'); ?>
+        <?php echo $form->beginControls(); ?>
+        <?php echo $form->textArea($model, 'body'); ?>
+        <?php echo $form->error($model, 'body'); ?>
+        <?php echo $form->endControls(); ?>
+        <?php echo $form->endControlGroup(); ?>
+    </div><!-- row -->
+<!--    <div class="row">
+        <?php echo $form->beginControlGroup($model, 'status'); ?>
+        <?php echo $form->labelEx($model, 'status'); ?>
+        <?php echo $form->beginControls(); ?>
+        <?php echo $form->textField($model, 'status', array('maxlength' => 45)); ?>
+        <?php echo $form->error($model, 'status'); ?>
+        <?php echo $form->endControls(); ?>
+        <?php echo $form->endControlGroup(); ?>
+    </div> row -->
+    <div class="row">
+        <?php echo $form->beginControlGroup($model, 'in_frontpage'); ?>
+        <?php echo $form->labelEx($model, 'in_frontpage'); ?>
+        <?php echo $form->beginControls(); ?>
+        <?php echo $form->checkBox($model, 'in_frontpage'); ?>
+        <?php echo $form->error($model, 'in_frontpage'); ?>
+        <?php echo $form->endControls(); ?>
+        <?php echo $form->endControlGroup(); ?>
+    </div><!-- row -->
 
-                <?php echo $form->textFieldRow($model,'status',array('class'=>'span5','maxlength'=>45)); ?>
-                <div class="row">
-		<?php // echo $form->labelEx($model,'in_frontpage'); ?>
-		<?php // echo $form->checkBox($model, 'in_frontpage'); ?>
-		<?php // echo $form->error($model,'in_frontpage'); ?>
-		</div><!-- row -->
-                <?php echo $form->checkBoxRow($model,'in_frontpage',array()); ?>
-                <?php echo $form->checkBoxRow($model,'in_blog',array()); ?>
-		<div class="row">
-		<?php //echo $form->labelEx($model,'created_at'); ?>
-		<?php //echo $form->textField($model, 'created_at'); ?>
-		<?php //echo $form->error($model,'created_at'); ?>
-		</div><!-- row -->
-		<div class="control-group">
-		<?php echo $form->labelEx($model,'publication_date', array('class'=>'control-label')); ?>
-		<?php
-                //echo $form->textField($model, 'publication_date', array('maxlength' => 45));
+    <div class="row">
+        <?php echo $form->beginControlGroup($model, 'publication_date'); ?>
+        <?php echo $form->labelEx($model, 'publication_date'); ?>
+        <?php echo $form->beginControls(); ?>
+        <?php
+        $form->widget('zii.widgets.jui.CJuiDatePicker', array(
+            'model'     => $model,
+            'attribute' => 'publication_date',
+            'value'     => $model->publication_date,
+            'options'   => array(
+                'showButtonPanel' => true,
+                'changeYear'      => true,
+                'dateFormat'      => 'yy-mm-dd',
+            ),
+            'htmlOptions'=>array(
+                'readonly'=>true,
+            ),
+        ));
+        ;
+        ?>
+<?php echo $form->error($model, 'publication_date'); ?>
+        <?php echo $form->endControls(); ?>
+        <?php echo $form->endControlGroup(); ?>
+    </div><!-- row -->
+    <div class="row">
+        <?php echo $form->beginControlGroup($model, 'in_blog'); ?>
+        <?php echo $form->labelEx($model, 'in_blog'); ?>
+        <?php echo $form->beginControls(); ?>
+        <?php echo $form->checkBox($model, 'in_blog'); ?>
+        <?php echo $form->error($model, 'in_blog'); ?>
+        <?php echo $form->endControls(); ?>
+        <?php echo $form->endControlGroup(); ?>
+    </div><!-- row -->
 
-                echo CHtml::openTag('div', array('class'=>'controls'));
-                $this->widget('zii.widgets.jui.CJuiDatePicker', array(
-                    'model'=>$model,
-                    'attribute'=>'publication_date',
-//                    'name'=>'Post[publication_date]',
-                    // additional javascript options for the date picker plugin
-                    'options'=>array(
-                        'showAnim'=>'fold',
-                        'changeMonth'=>true,
-                        'changeYear'=>true,
-                        'dateFormat'=>'yy-mm-dd',
-                        ),
-                    'language'=>'es',
-                    'htmlOptions'=>array(
-//                        'style'=>'height:20px;',
-                        'class'=>'span5',
-                    ),
-                ));
-                echo CHtml::closeTag('div');
-                ?>
-		<?php echo $form->error($model,'publication_date'); ?>
-		</div><!-- row -->
-		<div class="row">
-		<?php //echo $form->labelEx($model,'visits'); ?>
-		<?php //echo $form->textField($model, 'visits', array('maxlength' => 20)); ?>
-		<?php //echo $form->error($model,'visits'); ?>
-		</div><!-- row -->
+            <?php echo $form->beginControlGroup($model, 'categories'); ?>
 
-<!--		<label><?php echo GxHtml::encode($model->getRelationLabel('categories')); ?></label>-->
-		<?php echo $form->checkBoxListRow($model, 'categories', GxHtml::encodeEx(GxHtml::listDataEx(Category::model()->findAllAttributes(null, true)), false, true)); ?>
-            </div>
-        </div>
+    <label><?php echo GxHtml::encode($model->getRelationLabel('categories')); ?></label>
+            <?php echo $form->beginControls(); ?>
 
-<?php
-echo GxHtml::submitButton('Save');
-$this->endWidget();
-?>
-</fieldset>
+    <?php echo GxHtml::activecheckBoxList($model, 'categories', GxHtml::encodeEx(GxHtml::listDataEx(Category::model()->findAllAttributes(null, true)), false, true)); ?>
+<?php echo $form->endControls(); ?>
+        <?php echo $form->endControlGroup(); ?>
+     <?php echo $form->beginActions(); ?>
+        <?php echo $form->submitButton('Submit'); ?>
+    <?php echo $form->endActions(); ?>
+
+<?php $this->endWidget(); ?>
 </div><!-- form -->
-</div>
