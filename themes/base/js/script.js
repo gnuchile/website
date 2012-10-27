e71bdf5ec9ae3653d3d4a0d4d2a6f60adc636129
Tomás Solar Castro <tsolar@gnuchile.cl>
2012-10-27 13:55:24 -0300

    se pone código que debe cargar la página en script.js

--- themes/base/js/script.js
+++ themes/base/js/script.js
@@ -2,9 +2,18 @@
 
 */
 
-
-
-
-
-
-
+$(document).ready(function() {
+    $.featureList(
+        $("#tabs li a"),
+        $("#output li"), {
+            start_item	:	1
+        }
+        );
+/*
+                // Alternative
+                $('#tabs li a').featureList({
+                    output			:	'#output li',
+                    start_item		:	1
+                });
+                */
+});
