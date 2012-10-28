0ae411f2dcccf4ff96af2285b93423424a4ec1ed
Tomás Solar Castro <tsolar@gnuchile.cl>
2012-10-28 17:12:35 -0300

    se hace filtro trim para evitar guardar las fechas con espacios en blanco

--- protected/models/Date.php
+++ protected/models/Date.php
@@ -4,6 +4,18 @@ Yii::import('application.models._base.BaseDate');
 
 class Date extends BaseDate
 {
+    public function rules()
+    {
+        $rules = array(
+            array('start_date', 'filter', 'filter'=>'trim'),
+            array('start_time', 'filter', 'filter'=>'trim'),
+            array('end_date', 'filter', 'filter'=>'trim'),
+            array('end_time', 'filter', 'filter'=>'trim'),
+
+        );
+        return CMap::mergeArray(parent::rules(), $rules);
+    }
+
 	public static function model($className=__CLASS__) {
 		return parent::model($className);
 	}
